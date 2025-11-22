f1be82da167d0122f3012f9ceab3c5f9d7c35f42
Alidabi10 <112751131+Alidabi10@users.noreply.github.com>
2025-11-22 12:14:54 +0100

    lab_5
    Implement foo function to process a list of Human objects and return a vector of characters based on their properties.

--- foo.hpp
+++ foo.hpp
@@ -1,12 +1,28 @@
 #pragma once
 
 #include "Human.hpp"
-
 #include <list>
 #include <vector>
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector<char> wynik;
+    for (Human& person : people) {
+        person.birthday();
+        if (person.isMonster()) {
+            wynik.push_back('n');
+        }
+        else {
+            wynik.push_back('y');
+        }
+    }
+    size_t n = wynik.size();
+    for (size_t i = 0; i < n / 2; ++i) {
+        char chwilowo = wynik[i];
+        wynik[i] = wynik[n - 1 - i]; 
+        wynik[n - 1 - i] = chwilowo;
+    }
+
+
+    return wynik;
 }
